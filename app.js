90577aca4745eb979cc106c6a69c8f438af2270c
Carlo Santos <carlox7@gmail.com>
2017-02-21 14:50:26 -0800

    Made functions for random images

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 'strict';
+//html nodes
+var picContainer = document.getElementById('picture-container');
+var ItemPictureLeft = document.getElementById('left');
+var ItemPictureCenter = document.getElementById('center');
+var ItemPictureRight = document.getElementById('right');
+
+//click variables
+var totalClicks = 0;
+var clickLimit = 25;
+var itemsCurrentlyShowing = [];
+
 function CatalogItem (filePath, properties){
   this.filePath = filePath;
   this.properties = properties;
@@ -26,9 +37,8 @@ var babySweep = new CatalogItem('img/sweep.png','sweep');
 var tauntaunBag = new CatalogItem('img/tauntaun.jpg','tauntaun');
 var unicornCan = new CatalogItem('img/unicorn.jpg','unicorn');
 var tentacleUsb = new CatalogItem('img/usb.gif','usb');
-var wateringCan = new CatalogItem('img/watering-can.jpg','wateringCan');
+var wateringCan = new CatalogItem('img/water-can.jpg','wateringCan');
 var wineGlass = new CatalogItem('img/wine-glass.jpg','wineGlass');
-console.log(rTwoBag);
 
 //Item Array
 var items = [rTwoBag,bananaCutter,bathroomStand,rainBoots,breakfastMaker,meatGum,chairHump,cthulhuToy,dogDuck,dragonCan,penCap,petSweep,pizzaCutter,sharkBag,babySweep,tauntaunBag,unicornCan,tentacleUsb,wateringCan,wineGlass];
@@ -39,31 +49,71 @@ function randomPick(){
   return Math.floor(Math.random() * items.length);
 };
 
-var itemChoice = randomPick();
-console.log(itemChoice);
+//Stored random numbers
+var itemLeft = randomPick();
+var itemCenter = randomPick();
+var itemRight = randomPick();
+console.log(itemLeft);
+console.log(itemCenter);
+console.log(itemRight);
 
+var leftItemPicture = items[itemLeft];
+var centerItemPicture = items[itemCenter];
+var rightItemPicture = items[itemRight];
+console.log(leftItemPicture);
+console.log(centerItemPicture);
+console.log(rightItemPicture);
 //Aaray for random three picks
 var randomThreePicks = [];
-console.log(randomThreePicks);
 
 //Pick three random numbers to push to aaray
-// function itemPick(){
-//   var currentItem;
-//   for(var i = 0; i < 3; i++){
-//     currentItem = randomPick();
-//     randomThreePicks.push(currentItem);
-//   }
-// };
+function itemPick(){
+  for(var i = 0; i < 3; i++){
+    var currentItem = Math.floor(Math.random() * items.length);
+    randomThreePicks.push(currentItem);
+  }
+};
+itemPick();
+console.log(randomThreePicks);
 
 //Creates image node
-function drawCatalogItem(itemToDraw){
-  console.log(itemToDraw);
-  var imageEl = document.createElement('img');
-  var formEl = document.getElementById('product-pick');
-  formEl.appendChild(imageEl);
-  imageEl.src = itemToDraw.filePath;
+function drawLeftItem(itemToDraw){
+  var imageLeft = document.createElement('img');
+  var formLeft = document.getElementById('left');
+  formLeft.appendChild(imageLeft);
+  imageLeft.src = itemToDraw.filePath;
 };
 
-drawCatalogItem(wineGlass);
-drawCatalogItem(tentacleUsb);
-drawCatalogItem(pizzaCutter);
+function drawCenterItem(itemToDraw){
+  // console.log(itemToDraw);
+  var imageCenter = document.createElement('img');
+  var formCenter = document.getElementById('center');
+  formCenter.appendChild(imageCenter);
+  imageCenter.src = itemToDraw.filePath;
+};
+
+function drawRightItem(itemToDraw){
+  // console.log(itemToDraw);
+  var imageRight = document.createElement('img');
+  var formRight = document.getElementById('right');
+  formRight.appendChild(imageRight);
+  imageRight.src = itemToDraw.filePath;
+};
+//draws figure
+console.log(itemCenter);
+console.log(itemLeft);
+console.log(items[itemCenter]);
+console.log(items[itemLeft]);
+drawCenterItem(items[itemCenter]);
+drawLeftItem(items[itemLeft]);
+drawRightItem(items[itemRight]);
+
+
+// userInputStoreForm.addEventListener('click', handleClick);
+//
+// function handleClick(event) {
+//   event.preventDefault();
+//   event.stopPropagation();
+//
+//   var itemClick = event.target.productPick.value;
+//};
